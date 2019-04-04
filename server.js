e3cd136dc85d74a646bc694f54af60ec4b1b2295
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-05 00:42:57 +0300

    connect to a MongoDB Atlas Database

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    dbName: 'geopins'
+  })
+  .then(() => console.log('DB connected!'))
+  .catch(err => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
