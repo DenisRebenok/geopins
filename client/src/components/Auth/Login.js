0457589b2c79ef80f1d8674fad20987526909b2a
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-05 19:14:58 +0300

    Setted up Google OAuth & Added Google Login Button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,18 +1,30 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log({ idToken });
+  };
+
+  return (
+    <GoogleLogin
+      clientId="402948081568-in810vgg813vmvp5k48rji3kbgdtqb33.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 };
 
--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Login from '../components/Auth/Login';
 
 const Splash = () => {
-  return <div>Splash Page</div>;
+  return <Login />;
 };
 
 export default Splash;
