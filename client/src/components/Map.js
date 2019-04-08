a0612175c8f8dabaac5d6d5db0f5849d989125a4
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-09 00:46:08 +0300

    Created and Styled a Map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,41 +1,68 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiZGVueXNyZWJlbm9rIiwiYSI6ImNqdTh0anhpejBkNDYzeXBkeTlrbHFmbWoifQ.b0AzSTWSoBqYN7GAbXjTQQ"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      >
+        {/* Navigation Control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
   root: {
-    display: "flex"
+    display: 'flex'
   },
   rootMobile: {
-    display: "flex",
-    flexDirection: "column-reverse"
+    display: 'flex',
+    flexDirection: 'column-reverse'
   },
   navigationControl: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
-    margin: "1em"
+    margin: '1em'
   },
   deleteIcon: {
-    color: "red"
+    color: 'red'
   },
   popupImage: {
-    padding: "0.4em",
+    padding: '0.4em',
     height: 200,
     width: 200,
-    objectFit: "cover"
+    objectFit: 'cover'
   },
   popupTab: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
   }
 };
 
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Map from '../components/Map';
 import withRoot from '../withRoot';
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Map />
+    </>
+  );
 };
 
 export default withRoot(App);
