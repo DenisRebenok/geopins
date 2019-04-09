868941a6108e5858934b07b9dbeafee6324ebee9
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-09 18:31:09 +0300

    Building / Styling Blog Components

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,36 +1,87 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
-// import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SaveIcon from "@material-ui/icons/SaveTwoTone";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhotoTwoTone';
+import LandscapeIcon from '@material-ui/icons/LandscapeOutlined';
+import ClearIcon from '@material-ui/icons/Clear';
+import SaveIcon from '@material-ui/icons/SaveTwoTone';
 
 const CreatePin = ({ classes }) => {
-  return <div>CreatePin</div>;
+  return (
+    <form className={classes.form}>
+      <Typography
+        className={classes.alignCenter}
+        component="h2"
+        variant="h4"
+        color="secondary"
+      >
+        <LandscapeIcon className={classes.iconLarge} /> Pin Location
+      </Typography>
+      <div>
+        <TextField name="title" label="title" placeholder="Insert pin title" />
+        <input
+          accept="image/*"
+          id="image"
+          type="file"
+          className={classes.input}
+        />
+        <label htmlFor="image">
+          <Button component="span" size="small" className={classes.button}>
+            <AddAPhotoIcon />
+          </Button>
+        </label>
+      </div>
+      <div className={classes.contentField}>
+        <TextField
+          name="content"
+          label="Content"
+          multiline
+          rows="6"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <Button className={classes.button} variant="contained" color="primary">
+          <ClearIcon className={classes.leftIcon} />
+          Discard
+        </Button>
+        <Button
+          type="submit"
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+        >
+          Submit
+          <SaveIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 const styles = theme => ({
   form: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
     paddingBottom: theme.spacing.unit
   },
   contentField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: "95%"
+    width: '95%'
   },
   input: {
-    display: "none"
+    display: 'none'
   },
   alignCenter: {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center'
   },
   iconLarge: {
     fontSize: 40,
--- client/src/components/Pin/NoContent.js
+++ client/src/components/Pin/NoContent.js
@@ -1,20 +1,34 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Explore from "@material-ui/icons/Explore";
-// import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import ExploreIcon from '@material-ui/icons/Explore';
+import Typography from '@material-ui/core/Typography';
 
-const NoContent = ({ classes }) => <div>NoContent</div>;
+const NoContent = ({ classes }) => (
+  <div className={classes.root}>
+    <ExploreIcon className={classes.icon} />
+    <Typography
+      noWrap
+      component="h2"
+      variant="h6"
+      align="center"
+      color="textPrimary"
+      gutterBottom
+    >
+      Click on the map to add a pin
+    </Typography>
+  </div>
+);
 
 const styles = theme => ({
   root: {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "center"
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center'
   },
   icon: {
     margin: theme.spacing.unit,
-    fontSize: "80px"
+    fontSize: '80px'
   }
 });
 
