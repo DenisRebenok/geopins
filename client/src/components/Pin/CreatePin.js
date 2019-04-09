407f44bd57cfedb0d14ce08d74600b11ff738fc5
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-10 01:24:56 +0300

    Uploading Images with Cloudinary

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
@@ -23,9 +24,22 @@ const CreatePin = ({ classes }) => {
     dispatch({ type: 'DELETE_DRAFT' });
   };
 
-  const handleSubmit = event => {
+  const handleImageUpload = async () => {
+    const data = new FormData();
+    data.append('file', image);
+    data.append('upload_preset', 'geopins');
+    data.append('cloud_name', 'denrebenok');
+    const res = await axios.post(
+      'https://api.cloudinary.com/v1_1/denrebenok/image/upload',
+      data
+    );
+    return res.data.url;
+  };
+
+  const handleSubmit = async event => {
     event.preventDefault();
-    console.log({ title, image, content });
+    const url = await handleImageUpload();
+    console.log({ title, image, url, content });
   };
 
   return (
