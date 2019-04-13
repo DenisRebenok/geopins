24a1795230e0b78639598fd3674f543249bb32ea
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-13 21:15:55 +0300

    Handling Expired Auth Token Errors

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -27,6 +27,7 @@ const Login = ({ classes }) => {
 
   const onFailure = err => {
     console.error('Error logging in', err);
+    dispatch({ type: 'IS_LOGGED_IN', payload: false });
   };
 
   return (
